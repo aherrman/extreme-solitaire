5daec03cce22c5502a774f73d485983eb60cb346
Andy Herrman <aherrman@gmail.com>
2010-01-08 11:23:08 -0500

    Created ImmutableProxy

---
+++ lib/immutable_proxy.rb
@@ -0,0 +1,22 @@
+# Proxies any object but doesn't allow any mutating method (methods ending in
+# !) from being called.
+class ImmutableProxy
+  def initialize(target)
+    @target = target
+  end
+
+  def method_missing(name, *args, &block)
+    raise "Mutable methods not allowed" unless allowed?(name)
+    @target.send(name, *args, &block)
+  end
+
+  def respond_to?(name, include_private=false)
+    return false unless allowed?(name)
+    @target.respond_to?(name, include_private)
+  end
+
+private
+  def allowed?(name)
+    ! name.to_s.end_with?('!')
+  end
+end
---
+++ test/immutable_proxy_test.rb
@@ -0,0 +1,46 @@
+# Unit tests for ImmutableProxy
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'immutable_proxy'
+
+class ImmutableProxyTest < Test::Unit::TestCase
+
+  class TestMutableClass
+    def initialize(value)
+      @value = value
+    end
+
+    def get_value
+      @value
+    end
+
+    def set_value!(value)
+      @value = value
+    end
+  end
+
+  def setup
+    @mutable = TestMutableClass.new :initial_value
+    @proxy = ImmutableProxy.new @mutable
+  end
+
+  def test_proxy_calls_normal_method
+    assert_equal :initial_value, @proxy.get_value
+  end
+
+  def test_proxy_blocks_mutating_method
+    assert_raise(RuntimeError) do
+      @proxy.set_value!(:new_value)
+    end
+  end
+
+  def test_proxy_responds_to_normal_method
+    assert @proxy.respond_to?(:get_value)
+  end
+
+  def test_proxy_does_not_respond_to_mutating_method
+    assert ! @proxy.respond_to?(:set_value!)
+  end
+end
