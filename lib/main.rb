fd44d4a115c402f61340101fb8b8e840ccdce317
Andy Herrman <aherrman@gmail.com>
2010-01-29 23:46:43 -0500

    Added real command line arguments and parsing

--- lib/main.rb
+++ lib/main.rb
@@ -1,22 +1,65 @@
 require 'distance_from_solution_solver'
 require 'turn_count_solver'
 require 'solitaire_board'
+require 'optparse'
 
-def read_int_arg(arg, default=0)
-  value = arg
-  if value.nil?
-    value = default
-  else
-    value = Integer(value)
-  end
+def print_help(opts)
+  puts opts
+  exit
 end
 
-interactive = false
+options = {
+  :interactive => false,
+  :clear => false,
+  :shuffles => 0,
+  :solver => :distance,
+  :max_steps => nil
+}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: main.rb [options]"
+
+  opts.on('-i', '--interactive', 'Display the solution interactively') {
+    options[:interactive] = true
+  }
+
+  opts.on('-c', '--clear',
+      'Clears the console between each turn display when in interactive mode') {
+    options[:clear] = true
+  }
+
+  opts.on('-s', '--shuffles SHUFFLES', Integer,
+      'Number of times to shuffle the deck') { |s|
+    options[:shuffles] = s
+  }
+
+  opts.on('-m', '--max_steps MAX_STEPS', Integer,
+      'Maximum number of steps to run') { |m|
+    options[:max_steps] = m
+  }
+
+  opts.on('--solver SOLVER', [:distance, :turn],
+      'The solver to use.  Either distance or turn') { |s|
+    options[:solver] = s
+  }
+
+  opts.on('-h', '--help', 'Display usage') {
+    print_help opts
+  }
+end
+
+begin
+  optparse.parse ARGV
+rescue RuntimeError
+  print_help optparse
+end
+
+interactive = options[:interactive]
+clear = options[:clear]
+shuffle_count = options[:shuffles]
+max_steps = options[:max_steps]
 
 deck = StackOfCards.default_stack
-
-shuffle_count = read_int_arg ARGV[0]
-max_count = read_int_arg ARGV[1], nil
 
 deck.shuffle!(shuffle_count)
 board = SolitaireBoard.build_from_deck deck
@@ -24,12 +67,14 @@ board = SolitaireBoard.build_from_deck deck
 puts "Solving for board:"
 puts board.to_display_string
 
-#solver = TurnCountSolver.new board
-solver = DistanceFromSolutionSolver.new board
-solver.solve(max_count)
-#solver.solve(max_count) { |turn_count, queue_size, skipped|
-#  puts "Checking #{turn_count} - #{solver.processed} / #{queue_size} / #{skipped}"
-#}
+if options[:solver] == :turn
+  solver = TurnCountSolver.new board
+else
+  solver = DistanceFromSolutionSolver.new board
+end
+
+
+solver.solve(max_steps)
 
 if ! solver.solution_exists?
   puts "No solution exists\n"
@@ -37,16 +82,20 @@ else
   puts "Solution exists!"
 
   turns = solver.get_solution_turns
+  num_turns = turns[-1].board.turn_count
 
   turns.each do |turn|
-    print "\e[2J\e[f" if interactive
+    if interactive
+      STDIN.gets
+      print "\e[2J\e[f" if clear
+    end
+    puts "Turn ##{turn.board.turn_count} of #{num_turns}"
     puts turn.board.to_display_string
     puts turn.to_s
     puts '----------------------------------------------------------------------'
-    gets if interactive
   end
 
-  puts "Solved in #{turns[-1].board.turn_count}"
+  puts "Solved in #{num_turns}"
 end
 
 puts "Processed #{solver.processed} nodes"
